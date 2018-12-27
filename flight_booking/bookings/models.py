62586850ad072c1fbcb4608e3279858092978e31
Tosmak <tosmak16@gmail.com>
2018-12-27 11:49:20 +0100

    create custom user

--- flight_booking/bookings/models.py
+++ flight_booking/bookings/models.py
@@ -24,3 +24,31 @@ class UserManager(BaseUserManager):
         user.is_superuser = True
         user.save()
         return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+
+    email = models.EmailField(unique=True)
+    display_name = models.CharField(max_length=100, blank=True, null=True)
+    first_name = models.CharField(max_length=100, blank=True, null=True)
+    last_name = models.CharField(max_length=100, blank=True, null=True)
+    phone_number = models.IntegerField(blank=True, default=0)
+    date_joined = models.DateTimeField(default=timezone.now)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    service = models.CharField(max_length=200, blank=True, null=True)
+    location = models.CharField(max_length=100, blank=True, null=True)
+
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+
+    def __str__(self):
+        return f'@{self.email}'
+
+    def get_short_name(self):
+        return self.last_name
+
+    def get_long_name(self):
+        return f'{self.last_name} @{self.first_name}'
