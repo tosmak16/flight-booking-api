c64b3852e6860bbb8a2392ff9738c9420a1ea279
Tosmak <tosmak16@gmail.com>
2018-12-27 11:48:40 +0100

    create user manager

--- flight_booking/bookings/models.py
+++ flight_booking/bookings/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import (AbstractBaseUser, PermissionsMixin,
+                                        BaseUserManager)
+from django.contrib.auth.hashers import make_password
+from django.utils import timezone
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+
+    def create_user(self, email, password=None):
+
+        if email is None:
+            raise ValueError("Email is required")
+
+        user = self.model(
+            email=self.normalize_email(email),
+            password=make_password(password))
+        user.save()
+        return user
+
+    def create_superuser(self, email, password=None):
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save()
+        return user
