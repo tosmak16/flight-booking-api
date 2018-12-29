238e2148645cfb3dd6c99951677b68c0511689c9
Tosmak <tosmak16@gmail.com>
2018-12-29 13:32:20 +0100

    Add user sign up url

---
+++ flight_booking/apps/users/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url, include
+from rest_framework.routers import DefaultRouter
+
+from . import views
+
+router = DefaultRouter()
+
+router.register(r'users', views.UserDetailViewSet)
+
+urlpatterns = [
+    url(r'^', include(router.urls)),
+]
