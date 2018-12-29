1e5e1afc79b6197ff2a81c07db97618ab2dfbeec
Tosmak <tosmak16@gmail.com>
2018-12-29 15:53:37 +0100

    fix minor issues

--- flight_booking/apps/users/serializers.py
+++ flight_booking/apps/users/serializers.py
@@ -4,7 +4,9 @@ from django.contrib.auth import get_user_model
 
 
 class UserSerializer(serializers.ModelSerializer):
-    '''UserSerializer'''
+    """
+    UserSerializer
+    """
     password = serializers.CharField(
         write_only=True,
         min_length=9,
@@ -15,16 +17,30 @@ class UserSerializer(serializers.ModelSerializer):
         model = get_user_model()
         fields = ('id', 'first_name', 'last_name', 'email', 'password', 'phone_number', 'passport_url')  
 
-    def validate_password(self, password):
-        '''Validates password'''
-        if re.search(r'(^(?=.*[a-z].*[a-z])(?=.*[A-Z].*[A-Z])(?=.*\d.*\d)(?=.*\W.*\W)[a-zA-Z0-9\S]{9,}$)', password) is not None:
+    @staticmethod
+    def validate_password(password):
+        """
+        Validates password
+        :param password:
+        :return: password
+        """
+
+        if re.search(r'(^(?=.*[a-z].*[a-z])(?=.*[A-Z].*[A-Z'
+                     r'])(?=.*\d.*\d)(?=.*\W.*\W)[a-zA-Z0-9\S]{9,}$)', password) is not None:
             return password
         raise serializers.ValidationError(
             'Password value should have at least 2 uppercase, 2 lowercase, 2 digit and 2 special character.'
         )
-    def validate_email(self, email):
-        '''Validates email'''
-        if re.search(r'(^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$)', email) is not None:
+
+    @staticmethod
+    def validate_email(email):
+        """
+        Validates email
+        :param email:
+        :return:
+        """
+        if re.search(r'(^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]'
+                     r'{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$)', email) is not None:
             return email
         raise serializers.ValidationError(
             'Enter a valid email address.'
--- flight_booking/apps/users/views.py
+++ flight_booking/apps/users/views.py
@@ -1,23 +1,25 @@
-import jwt, os
-from rest_framework import viewsets, status, mixins
+from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
-from django.contrib.auth import authenticate
-from datetime import datetime, timedelta
 
 from .serializers import UserSerializer
 from .models import User
 
 
-
 class UserDetailViewSet(viewsets.GenericViewSet):
-    '''It handles user operations like sign up, sign in, update, retrieve'''
+
+    """ It handles user operations like sign up, sign in, update, retrieve """
+
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
     @action(methods=['POST'], detail=False)
     def signup(self, request):
-        '''It handles new user creation'''
+        """
+        It handles new user creation
+        :param request:
+        :return:
+        """
         serialized_user = UserSerializer(data=request.data)
         if serialized_user.is_valid():
             instance = serialized_user.save()
