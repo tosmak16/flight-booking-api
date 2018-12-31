b386b92758ff731e7226b9a8930bc0f3d9185558
Tosmak <tosmak16@gmail.com>
2018-12-31 14:59:29 +0100

    create auth.py file

---
+++ flight_booking/apps/users/auth.py
@@ -0,0 +1,36 @@
+import jwt, os
+from django.contrib.auth.models import User
+from .models import User
+from rest_framework import authentication, exceptions
+
+
+class VerifyToken(authentication.BaseAuthentication):
+    def authenticate(self, request):
+        token = request.META.get('HTTP_TOKEN') or request.META.get('token')
+        key = os.getenv('APP_SECRET_KEY')
+        email = None
+        try:
+            email = jwt.decode(token, key=key, algorithms=['HS256'], options={
+                        'verify_signature': True,
+                        'verify_exp': True
+
+            }).get('email')
+        except jwt.ExpiredSignatureError:
+            raise exceptions.AuthenticationFailed('Token has expired')
+        except jwt.InvalidAlgorithmError as error:
+            if str(error):
+                raise exceptions.AuthenticationFailed('User Authorization failed. Enter a valid token.')
+        except jwt.DecodeError as error:
+            if str(error) == 'Signature verification failed':
+                raise exceptions.AuthenticationFailed('Token Signature verification failed.')
+            else:
+                raise exceptions.AuthenticationFailed('Authorization failed due to an Invalid token.')
+
+        if not email:
+            return email
+        try:
+            user = User.objects.get(email=email)
+        except User.DoesNotExist:
+            raise exceptions.AuthenticationFailed('No such user')
+        return user, None
+
