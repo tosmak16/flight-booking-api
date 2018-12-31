128f43af082e04f12ecb4fbfcb01d49ca2376c57
Tosmak <tosmak16@gmail.com>
2018-12-31 15:02:37 +0100

    implement user upload, update and delete passport

--- flight_booking/apps/users/views.py
+++ flight_booking/apps/users/views.py
@@ -9,7 +9,7 @@ from datetime import datetime
 
 from .serializers import UserSerializer
 from .models import User
-from .helpers import GetAdminAndUserPermissions
+from .permissions import IsAdmin, IsOwner, PatchAdminAndUserPermissions
 from ...config.settings import TOKEN_EXP
 
 
@@ -19,7 +19,7 @@ class UserDetailViewSet(viewsets.GenericViewSet):
 
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = (GetAdminAndUserPermissions,)
+    permission_classes = (IsOwner, PatchAdminAndUserPermissions, )
     lookup_field = 'id'
 
     @action(methods=['POST'], detail=False)
@@ -61,3 +61,18 @@ class UserDetailViewSet(viewsets.GenericViewSet):
                             'exp': datetime.utcnow() + TOKEN_EXP},
                            os.getenv('APP_SECRET_KEY'), algorithm='HS256')
         return Response({'message': 'you have logged in successfully', 'token': token}, status=status_type)
+
+    @action(methods=['PATCH', 'DELETE'], detail=True)
+    def passport(self, request, id):
+        passport = request.data.get('passport')
+        if passport is None or len(str(passport).strip()) is 0 and request.method is "PATCH":
+            return Response({'message': 'passport field is required'}, status=status.HTTP_400_BAD_REQUEST)
+
+        user = User.objects.get(id=id)
+        user.passport_url.delete()
+        if request.method is "PATCH":
+            user.passport_url = passport
+            user.save()
+            return Response({'message': 'Passport Updated successfully'}, status=status.HTTP_200_OK)
+        user.save()
+        return Response({'message': 'Passport deleted successfully'}, status=status.HTTP_204_NO_CONTENT)
