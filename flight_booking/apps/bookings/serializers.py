5d29f2ad98ae86d16745c5441b0a4fe9d731f91a
Tosmak <tosmak16@gmail.com>
2019-01-02 08:10:20 +0100

    Add validators to booking serialzers

--- flight_booking/apps/bookings/serializers.py
+++ flight_booking/apps/bookings/serializers.py
@@ -20,8 +20,22 @@ class BookingSerializer(serializers.ModelSerializer):
         model = Booking
         fields = ('id', 'flight_from', 'flight_to', 'departing_date', 'returning_date', 'flight_type', 'owner')
 
+    def validate(self, data):
+        flight_type = str(data.get('flight_type'))
+        returning_date = data.get('returning_date')
 
+        if flight_type.upper() == 'ROUND' and returning_date is None:
+            raise serializers.ValidationError('returning_date is required')
+        if flight_type.upper() == 'ONE' and returning_date:
+            data['returning_date'] = None
+        return data
 
+    @staticmethod
+    def validate_flight_type(flight_type):
+        flight_type = str(flight_type).upper()
+        if flight_type not in ('ONE', 'ROUND'):
+            raise serializers.ValidationError('flight_type can only be ONE or ROUND')
+        return  flight_type
 
 
 
