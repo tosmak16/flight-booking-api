2a6b8aa9f2dbc0c7fb6b694b9fee3686cfa32a87
Tosmak <tosmak16@gmail.com>
2019-01-02 08:13:13 +0100

    Implement create and get list of bookings

--- flight_booking/apps/bookings/views.py
+++ flight_booking/apps/bookings/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import viewsets, mixins
 
-# Create your views here.
+from .serializers import BookingSerializer
+from .models import Booking
+from ..users.auth import VerifyToken
+from ..users.permissions import GetAdminPermissions, IsOwner
+
+
+class BookingsViewSet(mixins.ListModelMixin, viewsets.GenericViewSet, mixins.CreateModelMixin):
+    """ It handles flight bookings operations like booking a flight and getting list of booked flights """
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+    authentication_classes = (VerifyToken,)
+    permission_classes = (GetAdminPermissions,)
+
+
+
