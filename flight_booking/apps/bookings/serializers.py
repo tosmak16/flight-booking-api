acb84f465ded9b7c82920754d08ee555625e778e
Tosmak <tosmak16@gmail.com>
2019-01-02 11:45:22 +0100

    Add comments

--- flight_booking/apps/bookings/serializers.py
+++ flight_booking/apps/bookings/serializers.py
@@ -21,6 +21,11 @@ class BookingSerializer(serializers.ModelSerializer):
         fields = ('id', 'flight_from', 'flight_to', 'departing_date', 'returning_date', 'flight_type', 'owner')
 
     def validate(self, data):
+        """
+        Validates data
+        :param data: contains flight booking data
+        :return: data
+        """
         flight_type = str(data.get('flight_type'))
         returning_date = data.get('returning_date')
 
@@ -32,6 +37,11 @@ class BookingSerializer(serializers.ModelSerializer):
 
     @staticmethod
     def validate_flight_type(flight_type):
+        """
+        Validates flight_type
+        :param flight_type: contains flight_type value
+        :return: flight_type
+        """
         flight_type = str(flight_type).upper()
         if flight_type not in ('ONE', 'ROUND'):
             raise serializers.ValidationError('flight_type can only be ONE or ROUND')
