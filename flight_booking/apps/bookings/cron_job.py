fef9974e7686ccb6d916144464a2afe94cc6576f
Tosmak <tosmak16@gmail.com>
2019-01-03 23:26:49 +0100

    implement create automatic flight reminder email sender

---
+++ flight_booking/apps/bookings/cron_job.py
@@ -0,0 +1,21 @@
+from datetime import date, timedelta
+from django.core.mail import EmailMessage
+
+from .models import Booking
+from flight_booking.config.settings import DEFAULT_FROM_EMAIL
+
+
+def send_flight_reminder_mail():
+    tomorrow = date.today() + timedelta(days=1)
+    bookings = Booking.objects.filter(departing_date=tomorrow)
+    mass_email_list = []
+    if not len(bookings):
+        return bookings
+    for booking in bookings:
+        mass_email_list += [booking.owner.email]
+    msg=EmailMessage(
+        f"Flight reminder",
+        'Hello, this is a reminder that your flight is scheduled for ' + str(tomorrow),
+        DEFAULT_FROM_EMAIL,
+        to=mass_email_list)
+    msg.send()
