64cf429a9424c6631d550d61d6025bc193ce9917
Tosmak <tosmak16@gmail.com>
2019-01-14 09:34:07 +0100

    Update implementation

--- flight_booking/apps/bookings/views.py
+++ flight_booking/apps/bookings/views.py
@@ -5,11 +5,11 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.db import connection
 
-
-from .serializers import BookingSerializer
+from .serializers import BookingSerializer, BookingUpdateSerializer
 from .models import Booking
 from ..users.auth import VerifyToken
 from ..users.permissions import GetAdminPermissions, IsOwner
+from .helpers import handle_validate_and_update_booking
 
 
 class BookingsViewSet(mixins.ListModelMixin, viewsets.GenericViewSet, mixins.CreateModelMixin):
@@ -27,18 +27,32 @@ class BookingsDetailsViewSet(viewsets.GenericViewSet, mixins.UpdateModelMixin, m
     authentication_classes = (VerifyToken,)
     permission_classes = (IsOwner,)
 
+    lookup_field = 'id'
+
+    def update(self, request, id=None, *args, **kwargs):
+        """It updates bookings details
+
+        :param request: contains request data
+        :param id: booking id
+        :param args:
+        :param kwargs
+        :return: response message or data
+        """
+        serialized_booking_data = BookingUpdateSerializer(data=request.data)
+        return handle_validate_and_update_booking(request, serialized_booking_data, id)
+
 
 @api_view(['GET'])
 def users(request):
     date = request.query_params.get('date', None)
     if date is None:
         return Response(
-            {'message': 'date is required in the query param'},
+            {'message': 'date is required in the query param.'},
             status=status.HTTP_400_BAD_REQUEST
         )
     if re.search(r'(^([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]))$)', date) is None:
         return Response(
-            {'message': 'date is value should be YYYY-MM-DD format'},
+            {'message': 'Date has wrong format. Use one of these formats instead: YYYY-MM-DD.'},
             status=status.HTTP_400_BAD_REQUEST
         )
 
