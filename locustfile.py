d315428ed181697772aba639d07f21e907f4fe26
Tosmak <tosmak16@gmail.com>
2019-01-23 10:39:02 +0100

    Add book request handler

--- locustfile.py
+++ locustfile.py
@@ -3,10 +3,13 @@ from random import randint
 
 signup_url = 'http://0.0.0.0:8000/api/v1/users/signup/'
 signin_url = 'http://0.0.0.0:8000/api/v1/users/signin/'
+bookings = 'http://0.0.0.0:8000/api/v1/bookings/'
+
+fake_number = 51
 
 
 def signup(self):
-    x = self.client.post(signup_url, {
+    self.client.post(signup_url, {
         "email": 'tosmahzzky6{0}{1}@gmail.com'.format(randint(1, 1000), randint(0, 9999)),
         "password": "1717086207H#aAAaaz@@12",
         "first_name": "qq",
@@ -14,16 +17,21 @@ def signup(self):
     })
 
 
+def book(self):
+    self.client.post(signup_url, {
+        "flight_from": "Lagos",
+        "flight_to": "Abuja",
+        "flight_type": "ONE",
+        "owner": fake_number
+    })
+
+
 def login(self):
     self.client.post(signin_url, {"email": "tosmak16@gmail.com", "password": "1717086207H#aAAaaz@@12"})
 
 
-def index(self):
-    self.client.get("/")
-
-
 class UserBehavior(TaskSet):
-    tasks = {login: 1, index: 2}
+    tasks = {login: 1, book: 2}
 
     def on_start(self):
         signup(self)
